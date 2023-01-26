3d93aa00a06eb663ddce0d071751e5f47ec86678
Mario Anticoli <mario.anticoli@fcm.digital>
2023-01-26 21:07:57 +0100

    reorganise

--- test/include/DHTWrapperTest.h
+++
@@ -1,8 +0,0 @@
-//#include <cppunit/TestCase.h>
-//#include <cppunit/TestCaller.h>
-//#include <cppunit/ui/text/TestRunner.h>
-
-class DHTWrapperTest : public CppUnit::TestFixture {
-public:
-  void testAddition(void);
-};
--- test/lib/DHTWrapperTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <cppunit/TestCase.h>
-#include <cppunit/TestCaller.h>
-#include <cppunit/ui/text/TestRunner.h>
-#include "DHTWrapperTest.h"
-
-void DHTWrapperTest::testAddition() {
-  CPPUNIT_ASSERT(2 + 2 == 4);
-  CPPUNIT_ASSERT(2 + 2 == 3);
-}
