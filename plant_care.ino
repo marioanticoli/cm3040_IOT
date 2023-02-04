9c2fd187fdbddb0289be0b40593a3872992299c6
Mario Anticoli <mario.anticoli@fcm.digital>
2023-02-04 15:30:41 +0100

    Remove ControlPlantCare; check light and soil

--- plant_care.ino
+++ plant_care.ino
@@ -3,7 +3,6 @@
 #include "Arduino.h"
 #include "WebServer.h"
 #include "WifiCredentials.h"
-#include "ControlPlantCare.h"
 #include "DigitalPin.h"
 #include "AnalogPin.h"
 #include "DHTWrapper.h"
@@ -12,6 +11,8 @@
 #include "AnalogReader.h"
 #include "NCRelayController.h"
 #include "DigitalOutput.h"
+#include "Menu.h"
+#include "PlantSetting.h"
 
 #define DISPLAY_COLS 16
 #define DISPLAY_ROWS 2
@@ -27,12 +28,23 @@
 #define SOIL_SENSOR_PIN A0
 #define PHOTO_PIN A0
 
-#define DRY_VALUE 740
+#define DRY_VALUE 770
 #define WET_VALUE 288
 #define DARK_VALUE 1
 #define LIGHT_VALUE 1024
 
-#define LCD_PERIOD_MS 1000
+#define LCD_PERIOD_MS 500
+
+#define PLANT_SIZE 3
+
+enum class Action {
+  TOGGLE_LED,
+  TOGGLE_PUMP,
+  SET_PLANT,
+  SET_HUMIDITY,
+  SET_LUMINOSITY,
+  BACK,
+};
 
 uint now;
 // LCD last updated time
@@ -50,8 +62,14 @@ IRWrapper* ir;
 NCRelayController* pump;
 DigitalOutput* mux;
 DigitalOutput* led;
-ControlPlantCare* control;
+Menu<Action> *mainMenu;
+Menu<Action> *subMenu;
+Menu<Action> *currMenu;
 
+std::vector<PlantSetting> setting;
+bool menuActive;
+String line[2];
+uint currPlantSetting;
 
 void setup() {
   Serial.setDebugOutput(true);
@@ -67,7 +85,15 @@ void setup() {
   photo = new AnalogReader(PHOTO_PIN, DARK_VALUE, LIGHT_VALUE, false);
   mux = new DigitalOutput(MUX_PIN);
   led = new DigitalOutput(LED_PIN);
-  control = new ControlPlantCare(led, pump);
+
+  // Initialise menu
+  setting = std::vector<PlantSetting>(PLANT_SIZE);
+  Action arr1[3] = { Action::TOGGLE_LED, Action::TOGGLE_PUMP, Action::SET_PLANT };
+  mainMenu = new Menu<Action>(arr1, 3);
+  Action arr2[3] = { Action::SET_HUMIDITY, Action::SET_LUMINOSITY, Action::BACK };
+  subMenu = new Menu<Action>(arr2, 3);
+  currMenu = mainMenu;
+  menuActive = false;
 
   initWebServer();
 
@@ -86,28 +112,40 @@ void loop() {
   light = photo->get_perc_value();
   mux->toggle();
 
+  // Check if values for humidity and light reach current target
+  if(soilHum < setting[currPlantSetting].getHumidity()) {
+    pump->on();
+    delay(50);
+    pump->off();
+  }
+
+  if(light < setting[currPlantSetting].getLuminosity()) {
+    led->on();
+  } else {
+    led->off();
+  }
+
   // Listen to incoming requests to the webserver
   ws->listen();
 
-  // Check if IR received a command and pass it to the ControlPlantCare instance
+  // Check if IR received a command and react if valid
   // POWER turns ON/OFF lcd
   // EQ toggles the menu
-  // FUNC_STOP can be used to duplicate commands
   command = ir->getInput();
   if (command && command == IRWrapper::key::POWER) {
     lcd->toggle();
-  } else if (command && command == IRWrapper::key::EQ) {
+  } else if (command && command == IRWrapper::key::FUNC_STOP) {
     lcd->clear();
-    control->toggle();
-  } else if (command && control->isActive()) {
-    control->action(command);
+    toggleMenu();
+  } else if (command && menuActive) {
+    menuAction(command);
   }
+  command = 0;
 
   if (now - lcdLastUpdate >= LCD_PERIOD_MS) {
-    if (control->isActive()) {
-      auto l = control->getLines();
-      lcd->display(0, 0, l[0]);
-      lcd->display(1, 0, l[1]);
+    if (menuActive) {
+      lcd->display(0, 0, line[0]);
+      lcd->display(1, 0, line[1]);
     } else {
       lcd->display(0, 0, "Light: " + String(light) + "%");
       lcd->display(1, 0, "Soil: " + String(soilHum) + "%");
@@ -194,4 +232,100 @@ String setPump(String arg) {
 
 String statusStr(bool status, String output) {
   return String(output + " is currently " + status ? "ON" : "OFF");
+}
+
+/*
+*******************
+Menu block
+*******************
+*/
+
+void toggleMenu() {
+  menuActive = !menuActive;
+  currMenu = mainMenu;
+  mainMenu->reset();
+  subMenu->reset();
+  setMenuDisplay();
+}
+
+void setMenuDisplay() {
+  switch (currMenu->getItem()) {
+    case Action::TOGGLE_LED:
+      line[0] = String("Toggle LED");
+      line[1] = String(led->isActive() ? "ON -> OFF" : "OFF -> ON");
+      break;
+    case Action::TOGGLE_PUMP:
+      line[0] = String("Toggle pump");
+      line[1] = String(pump->isActive() ? "ON -> OFF" : "OFF -> ON");
+      break;
+    case Action::SET_PLANT:
+      line[0] = String("Set plant");
+      line[1] = String("'PLAY' + [1-3]");
+      break;
+    case Action::SET_HUMIDITY:
+      line[0] = String("Plant ") + String(currPlantSetting + 1);
+      line[1] = String("Hum. ") + String(setting[currPlantSetting].getHumidity()) + String("%");
+      break;
+    case Action::SET_LUMINOSITY:
+      line[0] = String("Plant ") + String(currPlantSetting + 1);
+      line[1] = String("Light ") + String(setting[currPlantSetting].getLuminosity()) + String("%");
+      break;
+    case Action::BACK:
+      line[0] = String("Go to main menu");
+      line[1] = String("");
+      break;
+  }
+}
+
+void execMenuHandler() {
+  switch (currMenu->getItem()) {
+    case Action::TOGGLE_LED:
+      led->toggle();
+      break;
+    case Action::TOGGLE_PUMP:
+      pump->toggle();
+      break;
+    case Action::SET_PLANT:
+      currMenu = subMenu;
+      break;
+    case Action::BACK:
+      currMenu->reset();
+      currMenu = mainMenu;
+      break;
+  }
+}
+
+void adjustValue(int8_t value) {
+  if (currMenu->getItem() == Action::SET_LUMINOSITY) {
+    setting[currPlantSetting].incLuminosity(value);
+  } else if (currMenu->getItem() == Action::SET_HUMIDITY) {
+    setting[currPlantSetting].incHumidity(value);
+  }
+}
+
+void menuAction(uint32_t command) {
+  switch (command) {
+    case IRWrapper::key::FAST_FORWARD:
+      currMenu->next();
+      break;
+    case IRWrapper::key::PLAY_PAUSE:
+      execMenuHandler();
+      break;
+    case IRWrapper::key::NR_1:
+      if (currMenu == subMenu) currPlantSetting = 0;
+      break;
+    case IRWrapper::key::NR_2:
+      if (currMenu == subMenu) currPlantSetting = 1;
+      break;
+    case IRWrapper::key::NR_3:
+      if (currMenu == subMenu) currPlantSetting = 2;
+      break;
+    case IRWrapper::key::DOWN:
+      if (currMenu == subMenu) adjustValue(-5);
+      break;
+    case IRWrapper::key::UP:
+      if (currMenu == subMenu) adjustValue(5);
+      break;
+  }
+  setMenuDisplay();
 }
